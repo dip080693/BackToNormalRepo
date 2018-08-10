37a87414942fde934e17a1221eb304ccd75cc529
dileep <dileep@192.168.0.4>
2018-08-11 00:17:35 +0530

    Next change

--- src/com/poc/test/InsertIntoGit.java
+++ src/com/poc/test/InsertIntoGit.java
@@ -5,5 +5,6 @@ public class InsertIntoGit {
 		System.out.println("Sample project to insert code into git");
 		System.out.println("Changed the currentn project");
 		System.out.println("Third change noticed");
+		System.out.println("Next change");
 	}
 }
