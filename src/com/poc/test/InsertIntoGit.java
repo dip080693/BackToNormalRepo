ae9cd50317808ec27e026815b509213aada36b53
dileep <dileep@192.168.0.4>
2018-08-11 00:12:48 +0530

    third commit

--- src/com/poc/test/InsertIntoGit.java
+++ src/com/poc/test/InsertIntoGit.java
@@ -4,5 +4,6 @@ public class InsertIntoGit {
 	public static void main(String[] args) {
 		System.out.println("Sample project to insert code into git");
 		System.out.println("Changed the currentn project");
+		System.out.println("Third change noticed");
 	}
 }
