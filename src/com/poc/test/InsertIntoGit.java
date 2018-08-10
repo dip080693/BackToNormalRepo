2088d1fa3e00919ad3845ceb286b193d084144c4
dileep <dileep@192.168.0.4>
2018-08-10 23:47:59 +0530

    Second Commit

--- src/com/poc/test/InsertIntoGit.java
+++ src/com/poc/test/InsertIntoGit.java
@@ -3,5 +3,6 @@ package com.poc.test;
 public class InsertIntoGit {
 	public static void main(String[] args) {
 		System.out.println("Sample project to insert code into git");
+		System.out.println("Changed the currentn project");
 	}
 }
